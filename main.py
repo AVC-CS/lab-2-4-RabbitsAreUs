34c8d1f925cc592ac5d01da4022d2574b78a0aeb
RabbitsAreUs <146285648+RabbitsAreUs@users.noreply.github.com>
2023-10-18 23:07:54 +0000

    Lab 4

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
 
-    original_str = 'Python Programming'
+    original_str = ['Python', 'Programming']
     ##################################################
     # Complete your code here
     ##################################################
     sub1 = original_str[0]
     sub2 = original_str[1]
-    merged_str = sub1 + sub2
+    merged_str = sub1 + ' ' + sub2
     print(sub2)
     print(sub1)
     print(merged_str)
