8d1454ddab6a29932965bde45ac267028dc7581e
RabbitsAreUs <146285648+RabbitsAreUs@users.noreply.github.com>
2023-10-18 23:26:29 +0000

    Lab 4

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     ##################################################
     sub1 = original_str[:6]
     sub2 = original_str[7:]
-    merged_str = sub1 + " " + sub2
+    merged_str = sub2 + " " + sub1
     print(sub2)
     print(sub1)
     print(merged_str)
